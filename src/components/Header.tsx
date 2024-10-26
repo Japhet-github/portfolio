c9db9842a10ae6a8d6616a1a8d77fa4a05adaf89
Japhet <vannjaphet@gmail.com>
2024-10-26 18:06:02 +0200

    initial header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+  <header className="bg-white dark:bg-gray-800 shadow">
+    <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-bold text-gray-900 dark:text-white">John Doe</h1>
+      <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">Junior Software Developer</p>
+    </div>
+  </header>
+  );
+}
